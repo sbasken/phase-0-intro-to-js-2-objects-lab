df3381f1e508aa329fa30d016c5496792a488799
Saki <sbasken@gmail.com>
2023-02-01 08:56:40 -0800

    add employee info, 4 functions to add and delete in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Saki",
+    streetAddress: "6006 3rd Ave.",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee}
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const keyDeleted = {...employee}
+    delete keyDeleted[key];
+    return keyDeleted
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
